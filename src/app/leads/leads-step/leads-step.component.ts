cbe117e2858d8b57eb145151d549930519f3329d
Serhii Hromov <melifarowow@gmail.com>
2022-05-03 00:31:10 +0300

    leads perfect loading

--- src/app/leads/leads-step/leads-step.component.ts
+++ src/app/leads/leads-step/leads-step.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { distinctUntilChanged, Observable, tap } from 'rxjs';
+import { delay, distinctUntilChanged, first, filter, tap, Subscription } from 'rxjs';
 import { Lead, ListFilter } from 'src/app/shared/model';
 import { ScrollService } from 'src/app/shared/scroll.service';
 import { AppState } from 'src/app/state/app.state';
@@ -15,23 +15,34 @@ import { selectFilteredLeads } from 'src/app/state/leads/leads.selector';
 export class LeadsStepComponent implements OnChanges, OnInit {
   @Input() step: number = null
   @Input() userID: number = null
-  leads$: Observable<Lead[]>
+  leads: Lead[]
   limit = 10
   offset = 0
   downloaded = 0
   canDownload = true
   loading: boolean
+  subs: Subscription[] = []
+
   constructor(
     private store: Store<AppState>,
     private scrollService: ScrollService,
   ) { }
 
   ngOnChanges(): void {
-    const filter: ListFilter = { limit: this.limit, offset: this.offset, step: this.step, responsible: this.userID }
-    this.store.dispatch(leadsRequired({ filter: filter }))
+    const lfilter: ListFilter = { limit: this.limit, offset: this.offset, step: this.step, responsible: this.userID }
+    this.store.dispatch(leadsRequired({ filter: lfilter }))
     this.loading = true
-    this.leads$ = this.store.select(selectFilteredLeads({ step: this.step, responsible: this.userID }))
-      .pipe(tap(leads => this.downloaded = leads && leads.length), tap(() => this.loading = false))
+    this.subs.forEach(s => s.unsubscribe())
+    this.subs.push(this.store.select(selectFilteredLeads({ step: this.step, responsible: this.userID }))
+      .pipe(
+        delay(3000),
+        tap(leads => this.downloaded = leads && leads.length),
+      ).subscribe(leads => {
+        this.leads = leads
+        this.loading = false
+      })
+    )
+
   }
   ngOnInit(): void {
     this.scrollService.percentage.pipe(distinctUntilChanged()).subscribe(p => {
@@ -42,7 +53,7 @@ export class LeadsStepComponent implements OnChanges, OnInit {
         this.canDownload = false
         this.offset += this.limit
         this.store.dispatch(leadsRequired({ filter: { limit: this.limit, offset: this.offset, step: this.step, responsible: this.userID } }))
-
+        this.loading = true
       }
     })
   }
