337825d3d0b5782583b9692b637f61f3c949a45d
Serhii Hromov <melifarowow@gmail.com>
2022-08-24 23:41:31 +0300

    fixes

--- src/app/login/auth.interceptor.ts
+++ src/app/login/auth.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {
-    HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse
 } from '@angular/common/http';
 
 import { Observable, interval, Subscription, map, catchError, throwError, first } from 'rxjs';
@@ -11,50 +11,52 @@ import { Router } from '@angular/router';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    authToken: string
-    refresher = interval(1000*60*50)
-    refresherSub: Subscription
+  authToken: string
+  refresher = interval(1000 * 60 * 50)
+  refresherSub: Subscription
 
-    intercept(req: HttpRequest<any>, next: HttpHandler):
-        Observable<any> {
-        if (this.authToken) {
-            const authReq = req.clone({
-                headers: req.headers.set('Authorization', `Bearer ${this.authToken}`)
-            })
-            return next.handle(authReq)
+  intercept(req: HttpRequest<any>, next: HttpHandler):
+    Observable<any> {
+    if (this.authToken) {
+      const authReq = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${this.authToken}`)
+      })
+      return next.handle(authReq)
+    }
+    return next.handle(req).pipe(
+      map((event: HttpEvent<any>) => {
+        if (event instanceof HttpResponse) {
+          console.log('event--->>>', event);
         }
-        return next.handle(req).pipe(
-            map((event: HttpEvent<any>) => {
-              if (event instanceof HttpResponse) {
-                console.log('event--->>>', event);
-              }
-              return event;
-            }),
-            catchError((error: HttpErrorResponse) => {
-              console.log(error.error.error);
-              if (error.status === 401) {
-                if (error.error.error === 'invalid_token') {
-                  this.auth.refreshToken().pipe(first()).subscribe(() => {
-                      location.reload();
-                    });
-                } else {
-                  this.router.navigate(['restricted']).then(_ => console.log('redirect to login'));
-                }
-              }
-              return throwError(error);
-            }));;
-    }
+        return event;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        console.log(error.error.error);
+        if (error.status === 401 || error.status === 403) {
+          this.auth.refreshToken().pipe(first())
+            .pipe(catchError((err) => {
+              console.log(err)
+              this.router.navigate(['restricted']).then(_ => console.log('redirect to login'));
+              return err
+            }))
+            .subscribe(() => {
+              location.reload();
+            });
+        }
+        return throwError(error);
+      }));;
+  }
 
-    constructor(private auth: AuthService, private router: Router) {
-        this.auth.socialUser$.subscribe(user => {
-            if (user) {
-                this.authToken = user.authToken
-                this.refresherSub = this.refresher.subscribe(() => this.auth.refreshToken().pipe(first()).subscribe())
+  constructor(private auth: AuthService, private router: Router) {
+    this.auth.socialUser$.subscribe(user => {
+      if (user) {
+        this.authToken = user.authToken
+        this.refresherSub = this.refresher.subscribe(() => this.auth.refreshToken().pipe(first()).subscribe())
 
-            } else {
-                this.authToken = ""
-                this.refresherSub.unsubscribe()
-            }
-        })
-    }
+      } else {
+        this.authToken = ""
+        this.refresherSub.unsubscribe()
+      }
+    })
+  }
 }
